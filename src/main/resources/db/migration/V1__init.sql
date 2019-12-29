330c0d445694637e0d05a15b2d538f10281d1758
madsasak <madsasak@yahoo-corp.jp>
2019-12-29 15:01:09 +0900

    SQL文修正

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,11 +1,11 @@
 CREATE TABLE todos (
   id int(255) NOT NULL AUTO_INCREMENT,
   name varchar(255) NOT NULL,
-  expire_date date ,
+  expire_at datetime ,
   status int(255) NOT NULL,
   is_deleted int(255) NOT NULL,
-  create_date date NOT NULL,
-  update_date date NOT NULL,
+  created_at datetime default current_timestamp NOT NULL,
+  updated_at timestamp default current_timestamp on update current_timestamp NOT NULL ,
   color_label varchar(255),
   PRIMARY KEY (id)utf8;
 ) ENGINE=InnoDB DEFAULT CHARSET=
