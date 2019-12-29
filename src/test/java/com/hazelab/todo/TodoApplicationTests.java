183cd5109f8ebc00826513d272771f3353573939
madsasak <madsasak@yahoo-corp.jp>
2019-12-29 11:45:59 +0900

    first commit🎉

---
+++ src/test/java/com/hazelab/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hazelab.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
