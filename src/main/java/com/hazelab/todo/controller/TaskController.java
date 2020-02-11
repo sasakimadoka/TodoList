3b4e5ac5eaa90cc34d34efbbdc780cc3110e6400
madsasak <madsasak@yahoo-corp.jp>
2020-02-11 22:30:02 +0900

    feature : index.htmlが確認可能

--- src/main/java/com/hazelab/todo/controller/TaskController.java
+++ src/main/java/com/hazelab/todo/controller/TaskController.java
@@ -1,14 +1,27 @@
 package com.hazelab.todo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class TaskController {
-    @GetMapping("task")
+    @GetMapping("/index")
     @ModelAttribute
     public ModelAndView index(ModelAndView model){
-        model.addObject("/task");
+        model.setViewName("index");//これなくても通る
         return model;
     }
+    @GetMapping("/create")
+    @ModelAttribute
+    public ModelAndView create(ModelAndView model){
+
+        return null;
+    }
+
+
+
+
+
 }
